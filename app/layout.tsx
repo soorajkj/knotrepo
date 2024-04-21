b8e6c330f3896579ae52a4a190be8c02e9d4e096
soorajkj <soorajkj46@gmail.com>
2024-04-21 12:08:16 +0530

    fix: :bug: manrop font load

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ interface LayoutProps extends Readonly<{ children: React.ReactNode }> {}
 
 export default function Layout(props: LayoutProps) {
   return (
-    <html lang="en" className={manrope.className} suppressHydrationWarning>
+    <html lang="en" className={manrope.variable} suppressHydrationWarning>
       <body className="h-full min-h-screen w-full bg-white font-family-manrope text-sm font-normal leading-normal text-zinc-700 antialiased dark:bg-zinc-950 dark:text-zinc-400">
         <ThemeProvider attribute="class" enableSystem defaultTheme="system">
           {props.children}
--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,7 +1,9 @@
 import { Manrope } from "next/font/google";
 
 export const manrope = Manrope({
+  variable: "--font-family-manrope",
   subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
   display: "swap",
   preload: true,
   style: ["normal"],
